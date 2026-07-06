5ef950718731ded580fd02bc5c0d396195d7af9e
Hao Bin Zhu <haobinzhu99@gmail.>
2026-07-06 10:23:46 -0400

    new dump.sql

--- data/dump.sql
+++ data/dump.sql
@@ -2,12 +2,12 @@
 -- PostgreSQL database dump
 --
 
-\restrict Lf46QqKI0W3QIkVT7sgMQoa6WhyGNgU34tSogr7UVt0btyBteTZfbckGSINC2BS
+\restrict SocfEqhJr7pIjgaV3PLW5rFCRt2Q8vezzrpnwdiegfMYPKmakz2cbgvFlbHuEUw
 
--- Dumped from database version 18.4 (Ubuntu 18.4-1.pgdg22.04+1)
--- Dumped by pg_dump version 18.3
+-- Dumped from database version 18.4 (Ubuntu 18.4-0ubuntu0.26.04.1)
+-- Dumped by pg_dump version 18.4
 
--- Started on 2026-07-06 09:50:53
+-- Started on 2026-07-06 10:13:11
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -26,76 +26,139 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 219 (class 1259 OID 16532)
+-- TOC entry 219 (class 1259 OID 16460)
 -- Name: Answer; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public."Answer" (
     id integer NOT NULL,
-    username text,
-    celebrity text
+    username text NOT NULL,
+    celebrity text NOT NULL,
+    "createdAt" timestamp(3) without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    "gameId" integer NOT NULL,
+    "normalizedAnswer" text NOT NULL
 );
 
 
 ALTER TABLE public."Answer" OWNER TO postgres;
 
 --
--- TOC entry 220 (class 1259 OID 16540)
+-- TOC entry 220 (class 1259 OID 16466)
 -- Name: Game; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public."Game" (
     id integer NOT NULL,
-    "roomCode" text,
-    answers text
+    "roomCode" text NOT NULL
 );
 
 
 ALTER TABLE public."Game" OWNER TO postgres;
 
 --
--- TOC entry 221 (class 1259 OID 16548)
--- Name: scoreboard; Type: TABLE; Schema: public; Owner: postgres
+-- TOC entry 221 (class 1259 OID 16513)
+-- Name: answer_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.scoreboard (
-);
+CREATE SEQUENCE public.answer_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
 
-ALTER TABLE public.scoreboard OWNER TO postgres;
+ALTER SEQUENCE public.answer_id_seq OWNER TO postgres;
 
 --
--- TOC entry 3362 (class 0 OID 16532)
+-- TOC entry 3494 (class 0 OID 0)
+-- Dependencies: 221
+-- Name: answer_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.answer_id_seq OWNED BY public."Answer".id;
+
+
+--
+-- TOC entry 222 (class 1259 OID 16521)
+-- Name: game_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.game_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.game_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 3495 (class 0 OID 0)
+-- Dependencies: 222
+-- Name: game_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.game_id_seq OWNED BY public."Game".id;
+
+
+--
+-- TOC entry 3329 (class 2604 OID 16520)
+-- Name: Answer id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Answer" ALTER COLUMN id SET DEFAULT nextval('public.answer_id_seq'::regclass);
+
+
+--
+-- TOC entry 3331 (class 2604 OID 16523)
+-- Name: Game id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Game" ALTER COLUMN id SET DEFAULT nextval('public.game_id_seq'::regclass);
+
+
+--
+-- TOC entry 3485 (class 0 OID 16460)
 -- Dependencies: 219
 -- Data for Name: Answer; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."Answer" (id, username, celebrity) FROM stdin;
+COPY public."Answer" (id, username, celebrity, "createdAt", "gameId", "normalizedAnswer") FROM stdin;
 \.
 
 
 --
--- TOC entry 3363 (class 0 OID 16540)
+-- TOC entry 3486 (class 0 OID 16466)
 -- Dependencies: 220
 -- Data for Name: Game; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."Game" (id, "roomCode", answers) FROM stdin;
+COPY public."Game" (id, "roomCode") FROM stdin;
 \.
 
 
 --
--- TOC entry 3364 (class 0 OID 16548)
+-- TOC entry 3496 (class 0 OID 0)
 -- Dependencies: 221
--- Data for Name: scoreboard; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Name: answer_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-COPY public.scoreboard  FROM stdin;
-\.
+SELECT pg_catalog.setval('public.answer_id_seq', 1, false);
 
 
 --
--- TOC entry 3212 (class 2606 OID 16539)
+-- TOC entry 3497 (class 0 OID 0)
+-- Dependencies: 222
+-- Name: game_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.game_id_seq', 1, false);
+
+
+--
+-- TOC entry 3333 (class 2606 OID 16476)
 -- Name: Answer Answer_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -104,7 +167,7 @@ ALTER TABLE ONLY public."Answer"
 
 
 --
--- TOC entry 3214 (class 2606 OID 16547)
+-- TOC entry 3335 (class 2606 OID 16478)
 -- Name: Game Game_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -112,11 +175,28 @@ ALTER TABLE ONLY public."Game"
     ADD CONSTRAINT "Game_pkey" PRIMARY KEY (id);
 
 
--- Completed on 2026-07-06 09:50:55
+--
+-- TOC entry 3336 (class 1259 OID 16524)
+-- Name: Game_roomCode_key; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX "Game_roomCode_key" ON public."Game" USING btree ("roomCode");
+
+
+--
+-- TOC entry 3337 (class 2606 OID 16525)
+-- Name: Answer Answer_gameId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Answer"
+    ADD CONSTRAINT "Answer_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES public."Game"(id) ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+-- Completed on 2026-07-06 10:13:11
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict Lf46QqKI0W3QIkVT7sgMQoa6WhyGNgU34tSogr7UVt0btyBteTZfbckGSINC2BS
+\unrestrict SocfEqhJr7pIjgaV3PLW5rFCRt2Q8vezzrpnwdiegfMYPKmakz2cbgvFlbHuEUw
 
