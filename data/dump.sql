b0c26a7f2e8f8a13381cc23b05473ecb4708274b
ABushUnderTheTree <hunterjah0@gmail.com>
2026-07-06 09:53:05 -0400

    trying a new backup method

--- data/dump.sql
+++ data/dump.sql
@@ -2,12 +2,12 @@
 -- PostgreSQL database dump
 --
 
-\restrict B4HiomdLmb1VgeWDtHzdUxunNY8f14M8wFPtznh6ooDhbWqHE5w5OYgrg4xw2aD
+\restrict Lf46QqKI0W3QIkVT7sgMQoa6WhyGNgU34tSogr7UVt0btyBteTZfbckGSINC2BS
 
 -- Dumped from database version 18.4 (Ubuntu 18.4-1.pgdg22.04+1)
 -- Dumped by pg_dump version 18.3
 
--- Started on 2026-07-02 11:11:16
+-- Started on 2026-07-06 09:50:53
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -20,25 +20,6 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-
---
--- TOC entry 4 (class 2615 OID 2200)
--- Name: public; Type: SCHEMA; Schema: -; Owner: pg_database_owner
---
-
-CREATE SCHEMA public;
-
-
-ALTER SCHEMA public OWNER TO pg_database_owner;
-
---
--- TOC entry 3365 (class 0 OID 0)
--- Dependencies: 4
--- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: pg_database_owner
---
-
-COMMENT ON SCHEMA public IS 'standard public schema';
-
 
 SET default_tablespace = '';
 
@@ -73,7 +54,18 @@ CREATE TABLE public."Game" (
 ALTER TABLE public."Game" OWNER TO postgres;
 
 --
--- TOC entry 3358 (class 0 OID 16532)
+-- TOC entry 221 (class 1259 OID 16548)
+-- Name: scoreboard; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.scoreboard (
+);
+
+
+ALTER TABLE public.scoreboard OWNER TO postgres;
+
+--
+-- TOC entry 3362 (class 0 OID 16532)
 -- Dependencies: 219
 -- Data for Name: Answer; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -83,7 +75,7 @@ COPY public."Answer" (id, username, celebrity) FROM stdin;
 
 
 --
--- TOC entry 3359 (class 0 OID 16540)
+-- TOC entry 3363 (class 0 OID 16540)
 -- Dependencies: 220
 -- Data for Name: Game; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -93,7 +85,17 @@ COPY public."Game" (id, "roomCode", answers) FROM stdin;
 
 
 --
--- TOC entry 3208 (class 2606 OID 16539)
+-- TOC entry 3364 (class 0 OID 16548)
+-- Dependencies: 221
+-- Data for Name: scoreboard; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.scoreboard  FROM stdin;
+\.
+
+
+--
+-- TOC entry 3212 (class 2606 OID 16539)
 -- Name: Answer Answer_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -102,7 +104,7 @@ ALTER TABLE ONLY public."Answer"
 
 
 --
--- TOC entry 3210 (class 2606 OID 16547)
+-- TOC entry 3214 (class 2606 OID 16547)
 -- Name: Game Game_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -110,11 +112,11 @@ ALTER TABLE ONLY public."Game"
     ADD CONSTRAINT "Game_pkey" PRIMARY KEY (id);
 
 
--- Completed on 2026-07-02 11:11:19
+-- Completed on 2026-07-06 09:50:55
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict B4HiomdLmb1VgeWDtHzdUxunNY8f14M8wFPtznh6ooDhbWqHE5w5OYgrg4xw2aD
+\unrestrict Lf46QqKI0W3QIkVT7sgMQoa6WhyGNgU34tSogr7UVt0btyBteTZfbckGSINC2BS
 
